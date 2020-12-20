f052f1edb81270fa50f6aa37e9cd4dd3becf30c5
Nuraiza <57836964+Nuraiza@users.noreply.github.com>
2020-12-20 07:05:04 +0100

    Add files via upload

---
+++ 6a/6a.js
@@ -0,0 +1,19 @@
+function ConvertToCelsius(form){
+
+var fValue = form.input.value;
+  var fToCel = (fValue - 32) * 5 / 9;
+  var message = fValue+'\xB0F is ' + fToCel + '\xB0C.';
+    console.log(message);
+    document.write (message);
+  document.body.style.backgroundColor = "white";
+
+}
+
+function ConvertToFahrenheit(form){
+var cValue = form.input.value;
+  var cToFahr = cValue * 9 / 5 + 32;
+  var message = cValue+'\xB0C is ' + cToFahr + ' \xB0F.';
+    console.log(message);
+     document.write (message);
+       document.body.style.backgroundColor = "white";
+}
