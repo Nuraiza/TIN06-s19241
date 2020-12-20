3342a52ca6ddef9a8882452ee644936373828649
Nuraiza <57836964+Nuraiza@users.noreply.github.com>
2020-12-20 08:52:02 +0100

    Add files via upload

---
+++ 6c/6c.js
@@ -0,0 +1,76 @@
+function validateData(form){
+var fname = form.fname.value;
+var lname = form.lname.value;
+var tnumber = form.tnumber.value;
+
+
+ 
+ if (fname === "") {
+    console.log("All fields must be filled ");
+    document.getElementById('fname').focus();
+    return false;
+  }
+  if(lname===""){
+   console.log("All fields must be filled ");
+    document.getElementById('lname').focus();
+    return false;
+  }
+  if(tnumber===""){
+    console.log("All fields must be filled ");
+    document.getElementById('tnumber').focus();
+    return false;
+  }
+ if(nameTest(fname)===false){
+  console.log("Not valid first name");
+  document.getElementById('fname').focus();
+  return false;
+  
+ }
+ if(nameTest(lname)===false){
+ console.log("Not valid last name");
+ document.getElementById('lname').focus();
+ return false;
+ 
+ }
+if(allNumeric(tnumber)===false){
+ console.log("Not valid telephone number");
+ document.getElementById('tnumber').focus();
+ return false;
+
+
+}
+console.log("Correct");
+return true;
+
+
+}
+
+
+
+function nameTest(name){
+var regName = /^[a-zA-Z]+$/;
+if(regName.test(name)){
+   return true;
+}else{
+   return false;
+}
+}
+
+function allNumeric(number){
+var numberReg = /^[0-9]+$/;
+      if(number.match(numberReg))
+      {
+      return true;
+}else{
+return false;
+}
+
+}
+
+
+
+
+
+
+
+
---
+++ 6d/6c.js
@@ -0,0 +1,92 @@
+function validateData(form){
+var fname = form.fname.value;
+var lname = form.lname.value;
+var tnumber = form.tnumber.value;
+
+
+ 
+ if (fname === "") {
+    console.log("All fields must be filled ");
+    document.getElementById('fname').focus();
+    return false;
+  }
+  if(lname===""){
+   console.log("All fields must be filled ");
+    document.getElementById('lname').focus();
+    return false;
+  }
+  if(tnumber===""){
+    console.log("All fields must be filled ");
+    document.getElementById('tnumber').focus();
+    return false;
+  }
+ if(nameTest(fname)===false){
+  console.log("Not valid first name");
+  document.getElementById('fname').focus();
+  return false;
+  
+ }
+ if(nameTest(lname)===false){
+ console.log("Not valid last name");
+ document.getElementById('lname').focus();
+ return false;
+ 
+ }
+if(allNumeric(tnumber)===false){
+ console.log("Not valid telephone number");
+ document.getElementById('tnumber').focus();
+ return false;
+
+
+}
+console.log("Correct");
+return true;
+
+
+}
+
+
+
+function nameTest(name){
+var regName = /^[a-zA-Z]+$/;
+if(regName.test(name)){
+   return true;
+}else{
+   return false;
+}
+}
+
+function allNumeric(number){
+var numberReg = /^[0-9]+$/;
+      if(number.match(numberReg))
+      {
+      return true;
+}else{
+return false;
+}
+
+}
+
+
+function addRow(value){
+var table = document.getElementById("myTable");
+  var row = table.insertRow(0);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  cell1.innerHTML = "Gender";
+  var input = document.createElement("input");
+                input.type = "text";
+                input.id = "gender";
+                input.name = "gender";
+                input.value="";
+ 
+ cell2.appendChild(input);
+  
+
+
+}
+
+
+
+
+
